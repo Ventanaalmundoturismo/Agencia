06375b2a85ff0b26807dd9c52ac723c5e507f396
PachaNEG69 <Emgalindezpachanahuel35@gmail.com>
2022-09-05 02:17:00 -0300

    Caza

---
+++ app.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+navMenu.classList.toggle("nav-menu_visible");
+});
+
+const RedesToggle = document.querySelector(".nav-toggle");
+const Redes = document.querySelector(".Redes");
+
+navToggle.addEventListener("click", () => {
+Redes.classList.toggle("Redes_visible");
+});
