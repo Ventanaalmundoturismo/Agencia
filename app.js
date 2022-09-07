ea4dff7b065dabef449b17d89c01b9f94239f76a
PachaNEG69 <Emgalindezpachanahuel35@gmail.com>
2022-09-07 04:47:35 -0300

    Mejorando la seccion Nosotros

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ const Redes = document.querySelector(".Redes");
 navToggle.addEventListener("click", () => {
 Redes.classList.toggle("Redes_visible");
 });
+
+
+
+const acordeon = document.getElementsByClassName("contenedor-box");
+
+for (i = 0; i<acordeon.length; i++ ){
+   acordeon[i].addEventListener("click", function(){
+    this.classList.toggle("active")
+   })
+}
