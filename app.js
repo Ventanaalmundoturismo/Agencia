8be2a8f8ad47cf0170b9eab76ca699b3e53d3471
PachaNEG69 <Emgalindezpachanahuel35@gmail.com>
2022-09-22 02:40:51 -0300

    ARREGLOS GENERALES

--- app.js
+++ app.js
@@ -1,53 +1,23 @@
-let toggle = document.getElementById("toggle");
+let toggle = document.getElementById('toggle');
 toggle.onclick = function () {
-    toggle.classList.toggle("active")
+    toggle.classList.toggle('active')
 };
 
 
 
-const navToggle = document.querySelector("#toggle");
-const navMenu = document.querySelector(".nav-menu");
+const navToggle = document.getElementById('toggle');
+const navMenu = document.querySelector('.nav-menu');
 
-navToggle.addEventListener("click", () => {
-navMenu.classList.toggle("nav-menu_visible");
-});
-
-const RedesToggle = document.querySelector(".nav-toggle");
-const Redes = document.querySelector(".Redes");
-
-navToggle.addEventListener("click", () => {
-Redes.classList.toggle("Redes_visible");
+navToggle.addEventListener('click', () => {
+navMenu.classList.toggle('nav-menu_visible');
 });
 
 
 
-const acordeon = document.getElementsByClassName("contenedor-box");
+const acordeon = document.getElementsByClassName('contenedor-box');
 
 for (i = 0; i<acordeon.length; i++ ){
-   acordeon[i].addEventListener("click", function(){
-    this.classList.toggle("active")
+   acordeon[i].addEventListener('click', function(){
+    this.classList.toggle('active')
    })
 }
-
-
-
-let box = document.querySelectorAll(".box");
-box.forEach(popup => popup.addEventListener("click", () => {
-    popup.classList.toggle("active")
-}))
-
-
-
-/*
-let list = document.querySelectorAll(".Redes a");
-let bg = document.querySelector("body");
-list.forEach(elements => {
-   elements.addEventListener("mouseenter", function(event){
-      let color = event.target.getAttribute("data-color");
-      bg.style.backgroundColor = color;
-   })
-   elements.addEventListener("mouseleave", function(event){
-      bg.style.backgroundColor = "#83c5f7";
-   })
-   
-})*/
